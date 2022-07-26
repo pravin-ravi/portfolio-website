e1cbc1dcf1982c1eca6bc39613ceec4325d3e371
rishav <rishavbhardwaj82@gmail.com>
2022-07-27 02:52:18 +0530

    changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,8 +8,8 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/dev26rishav/portfolio-master/master/src/Assets/CV_RISHAV.pdf";
+const resumeLink = 
+   "https://raw.githubusercontent.com/dev26rishav/portfolio-master/master/src/Assets/CV_RISHAV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
