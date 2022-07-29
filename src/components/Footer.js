7418947293a983912c25292021f395a5feb2f154
Pravin kumar Ravi <btech10426.19@bitmesra.ac.in>
2022-07-30 03:07:23 +0530

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,11 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
           <h3>Designed and Developed by Pravin Ravi </h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} DR</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
